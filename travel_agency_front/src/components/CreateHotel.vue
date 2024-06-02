ef74747f195caa5a2c17c31663182bd96e91f542
SodolynskyiVadym <vadimsodolinskij@gmail.com>
2024-06-02 12:27:39 +0300

    final CreatePlace page

--- travel_agency_front/src/components/CreateHotel.vue
+++ travel_agency_front/src/components/CreateHotel.vue
@@ -1,7 +1,29 @@
 <template>
     <h1>Create Hotel</h1>
+    <label for="search">Search:</label>
+    <input list="data" id="search" type="search" v-model="searchTerm" @input="test" />
+    <datalist id="data">
+        <option v-for="item in dataList" :key="item" :value="item">
+            {{ item }}
+        </option>
+    </datalist>
 </template>
 
 <script>
+import { countries } from '@/js/countries';
+export default {
+    data() {
+        return {
+            searchTerm: '',
+            dataList: countries
+        }
+    },
+
+    methods: {
+        async test() {
+            console.log(this.searchTerm);
+        }
+    }
+}
 
 </script>
--- travel_agency_front/src/components/CreatePlace.vue
+++ travel_agency_front/src/components/CreatePlace.vue
@@ -2,13 +2,14 @@
     <h1 style="margin-top: 30px;">Create Place</h1>
     <div class="create-form">
         <label for="name">Place Name:</label>
-        <input id="name" type="text" v-model="name" placeholder="Enter place name">
+        <input id="name" type="text" v-model="name" placeholder="Enter place name" @input="checkCorrectInputs">
         <div class="error" v-if="!name">Name is required</div>
 
         <div class="create-form" v-for="(imageUrl, index) in imagesUrls" :key="index">
             <div>
                 <label :for="'imageUrl' + index">Image URL:</label>
-                <input :id="'imageUrl' + index" type="text" v-model="imagesUrls[index]" @input="validateImageUrl(index)" required />
+                <input :id="'imageUrl' + index" type="text" v-model="imagesUrls[index]" @input="validateImageUrl(index)"
+                    required />
                 <button style="margin-left: 30px;" type="button" @click="removeImage(index)">Remove</button>
             </div>
 
@@ -16,21 +17,28 @@
             <img v-else style="width: 300px;" class="image-preview" src='./@/../../assets/images/image-not-found.jpg' />
         </div>
         <button v-if="!(imagesUrls.length >= 3)" type="button" @click="addImage">Add image</button>
-        <div class="error" v-if="!isAllImagees">Place must have 3 images</div>
+        <div class="error" v-if="!isAllImages">Place must have 3 images</div>
 
-        <label for="country">Country:</label>
-        <select id="country" v-model="country">
+        <input type="search" list="data" class="form-control" @input="checkCountryFromList" autocomplete="false"
+            autocorrect="on" v-model="country" placeholder="Type to choose country">
+        <datalist id="data">
             <option v-for="country in countries" :key="country" :value="country">
                 {{ country }}
             </option>
-        </select>
-        <div class="error" v-if="!country">Country is required</div>
+        </datalist>
+        <div class="error" v-if="!isCountryFromList">Country is required</div>
+        
 
         <label for="description">Description:</label>
-        <textarea id="description" v-model="description" placeholder="Enter place description"></textarea>
+        <textarea id="description" v-model="description" placeholder="Enter place description"
+            @input="checkCorrectInputs"></textarea>
         <div class="error" v-if="!description">Description is required</div>
 
-        <button style="margin-top: 20px;" @click="createPlace">Create Place</button>
+        <button v-if="!isSendRequest" style="margin: 20px;" @click="createPlace" :disabled="!isCorrectInputs">Create Place</button>
+        <button v-else style="background-color: #4CAF50; margin: 20px;" class="btn btn-primary" type="button" disabled>
+            <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+            <span role="status">Loading...</span>
+        </button>
     </div>
 </template>
 
@@ -48,11 +56,24 @@ export default {
             existImages: [false],
             isCorrectImage: false,
             countries: countries,
-            isAllImages: false
+            isAllImages: false,
+            isCorrectInputs: false,
+            isCountryFromList: false,
+            isSendRequest: false
         }
     },
 
     methods: {
+        async checkCorrectInputs() {
+            this.isCorrectInputs = this.name && this.isCountryFromList && this.description && this.isAllImages;
+            console.log(this.isCorrectInputs);
+        },
+
+        async checkCountryFromList() {
+            this.isCountryFromList = this.countries.includes(this.country);
+            await this.checkCorrectInputs();
+        },
+
         async clearFields() {
             this.name = '';
             this.country = '';
@@ -69,8 +90,12 @@ export default {
                 imagesUrls: this.imagesUrls
             }
 
+            this.isSendRequest = true;
             await placeAPI.createPlace(data);
             await this.clearFields();
+            await this.checkCountryFromList();
+            await this.validateImageUrl();
+            this.isSendRequest = false;
         },
 
         async addImage() {
@@ -97,7 +122,8 @@ export default {
 
         async validateImageUrl(index) {
             this.existImages[index] = await this.checkImageExists(this.imagesUrls[index]);
-            this.isAllImagees = this.existImages.every((existImage) => existImage) && this.imagesUrls.length === 3;
+            this.isAllImages = this.existImages.every((existImage) => existImage) && this.imagesUrls.length === 3;
+            await this.checkCorrectInputs();
         }
     }
 }
