3b1fcad383f5472a3fb6af3ea453923dc285222d
SodolynskyiVadym <vadimsodolinskij@gmail.com>
2024-07-26 11:33:38 +0300

    update docker file

--- database/init.sql
+++
@@ -1,9 +0,0 @@
-IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'travel_agency')
-BEGIN
-    CREATE DATABASE travel_agency;
-    PRINT 'Database created';
-END
-ELSE 
-BEGIN
-    PRINT 'Database already exists';
-END
