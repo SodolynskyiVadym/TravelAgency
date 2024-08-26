d4678f6992238799a7b0de69cdc98b74a524c3ec
SodolynskyiVadym <vadimsodolinskij@gmail.com>
2024-08-26 22:19:28 +0300

    update serverUrl

--- travel_agency_front/src/js/serverUrl.js
+++ travel_agency_front/src/js/serverUrl.js
@@ -1,7 +1,9 @@
 ﻿let serverUrl;
-if (process.env.SERVER_ADDRESS && process.env.SERVER_PORT) {
-    serverUrl = `${process.env.SERVER_ADDRESS}:${process.env.SERVER_PORT}`;
-} else {
+if (process.env.ENVIROMENT === "DockerEnv") {
+    serverUrl = process.env.SERVER_ADDRESS;
+}else if (process.env.ENVIROMENT === "ProductionEnv") {
+    serverUrl = process.env.SERVER_ADDRESS;
+}else {
     serverUrl = process.env.SERVER_DEFAULT_ADDRESS || "http://localhost:5113";
 }
 
