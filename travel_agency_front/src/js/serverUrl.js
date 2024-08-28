42ee420854df11b050ed593eaf603d38158cdcf2
SodolynskyiVadym <vadimsodolinskij@gmail.com>
2024-08-28 14:56:51 +0300

    update serverUrl

--- travel_agency_front/src/js/serverUrl.js
+++ travel_agency_front/src/js/serverUrl.js
@@ -1,11 +1,11 @@
-﻿// let serverUrl;
-// if (process.env.ENVIROMENT === "DockerEnv") {
-//     serverUrl = process.env.SERVER_ADDRESS;
-// }else if (process.env.ENVIROMENT === "ProductionEnv") {
-//     serverUrl = process.env.SERVER_ADDRESS;
-// }else {
-//     serverUrl = process.env.SERVER_DEFAULT_ADDRESS || "http://localhost:5113";
-// }
-let serverUrl = "https://travelagencyapi-drb5fuezevd5hhej.germanywestcentral-01.azurewebsites.net";
+﻿let serverUrl;
+if (process.env.ENVIROMENT === "DockerEnv") {
+    serverUrl = process.env.SERVER_ADDRESS;
+}else if (process.env.ENVIROMENT === "Production") {
+    serverUrl = process.env.SERVER_ADDRESS;
+}else {
+    serverUrl = process.env.SERVER_DEFAULT_ADDRESS || "http://localhost:5113";
+}
+// let serverUrl = "https://travelagencyapi-drb5fuezevd5hhej.germanywestcentral-01.azurewebsites.net";
 
 export default serverUrl;
