8f6ffdad3e609364e8c24040c78c08a072db58f0
SodolynskyiVadym <vadimsodolinskij@gmail.com>
2024-10-21 11:05:36 +0300

    changed directory of vue project

--- travel_agency_site/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { TestComponentComponent } from './test-component/test-component.component';
-
-export const routes: Routes = [
-    {path: 'test', component: TestComponentComponent},
-];
